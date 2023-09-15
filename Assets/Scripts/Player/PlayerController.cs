6bbcc57853e5d7344dee9a42870634523fce4c9b
xiaoxizi <xiaoxizi@xiaoxizideMBP.lan>
2023-09-15 18:45:38 +0800

    +Add inputSystem

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerInputControl inputControl;
+    public Vector2 inputDirection;
+
+    private void Awake()
     {
-        
+        inputControl = new PlayerInputControl(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
     }
 }
