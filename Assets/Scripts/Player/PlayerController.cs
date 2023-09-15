9a14c5c5afd20eee10c134d43dd81611c8506ea1
YuHui <34625909+FoolButIntelligent@users.noreply.github.com>
2023-09-16 04:12:56 +0800

    +Add player horizontal movement & velocity

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
     public Vector2 inputDirection;
+    public float speed;
+    public Rigidbody2D rb;
+    private SpriteRenderer rbSprite;
 
     private void Awake()
     {
         inputControl = new PlayerInputControl(); 
+        rbSprite = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -26,5 +30,23 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime,rb.velocity.y);
+
+        //人物面朝方向
+
+        if (inputDirection.x > 0)
+            rbSprite.flipX = false;
+        if(inputDirection.x < 0)
+            rbSprite.flipX = true;
+        //人物翻转
     }
 }
