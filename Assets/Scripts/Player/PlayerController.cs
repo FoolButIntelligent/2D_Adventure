30b14b8534318bc391643f99cd7132b4e3e90a32
YuHui <34625909+FoolButIntelligent@users.noreply.github.com>
2023-09-27 18:56:38 +0800

    +Add attack judgement(p1)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -81,7 +81,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!isHurt)
+        if (!isHurt && !isAttack)
         {
             Move();
         }  
