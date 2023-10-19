b9dde0d281fc18ae2d7536d974a5d478704439b5
YuHui <34625909+FoolButIntelligent@users.noreply.github.com>
2023-10-19 14:46:51 +0800

    +Add  camera shake

--- Assets/Scripts/Utilities/CameraControl.cs
+++ Assets/Scripts/Utilities/CameraControl.cs
@@ -2,16 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using System;
 
 public class CameraControl : MonoBehaviour
 {
     private CinemachineConfiner2D confiner2D;
+    public CinemachineImpulseSource impulseSource;
+    public VoidEventSO cameraShakeEvent;
 
     private void Awake()
     {
         confiner2D = GetComponent<CinemachineConfiner2D>();
     }
 
+    private void OnEnable()
+    {
+        cameraShakeEvent.OnEventRaised += OnCameraShakeEvemt;
+    }
+
+    private void OnDisable()
+    {
+        cameraShakeEvent.OnEventRaised -= OnCameraShakeEvemt;
+    }
+
+    private void OnCameraShakeEvemt()
+    {
+        impulseSource.GenerateImpulse();
+    }
+
+    private void Start()
+    {
+        GetNewCameraBounds();
+    }
 
     private void GetNewCameraBounds()
     {
@@ -21,6 +43,7 @@ public class CameraControl : MonoBehaviour
             return;
 
         confiner2D.m_BoundingShape2D = obj.GetComponent<Collider2D>();
+        confiner2D.InvalidateCache();
     }
 
 }
